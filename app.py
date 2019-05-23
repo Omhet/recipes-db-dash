4a42586d1a7e8e545e101113f93bb23d0fbe6db0
Vladimir Ivanov <vlivanov@aligntech.com>
2019-05-23 11:06:01 +0300

    Remove API using

--- app.py
+++ app.py
@@ -16,51 +16,26 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-showcase_recipes = []
-
-
-def fetch_showcase_recipes():
-    url = 'https://recipes-search.glitch.me/showcase'
-    response = requests.get(url)
-    data = response.json()
-    return data['recipes']
-
-
-showcase_recipes = fetch_showcase_recipes()
-
 app.layout = html.Div(className='flex', children=[
-    dcc.Tabs(id="tabs", children=[
-        dcc.Tab(label='Найти', children=[
-            html.Div(className='flex', children=[
-                dcc.Input(
-                    placeholder='Найти блюдо...',
-                    type='text',
-                    value='',
-                    id='recipe-search'
-                ),
-                html.Div(id='found-recipes-container')
-            ])
-        ]),
-        dcc.Tab(label='Мои рецепты', children=[
-            html.Div(className='flex-wide', children=[
-                html.Div(id='user-recipe-placeholder', className='recipe'),
-                html.Div(id='add-recipe-form', children=[
+    html.Div(className='flex-wide', children=[
+        html.Div(id='user-recipe-placeholder', className='recipe'),
+        html.Div(id='add-recipe-form', children=[
                     html.Label('Название', htmlFor='recipe-title'),
                     dcc.Input(
                         placeholder='Омлет',
                         type='text',
                         id='recipe-title'
                     ),
-                    html.Label('Ссылка на изображение',
-                               htmlFor='recipe-image'),
-                    dcc.Input(
+            html.Label('Ссылка на изображение',
+                       htmlFor='recipe-image'),
+            dcc.Input(
                         placeholder='htttp://example.com/?img=123',
                         type='text',
                         value='',
                         id='recipe-image'
                     ),
-                    html.Label('Количество порций', htmlFor='recipe-portions'),
-                    dcc.Input(
+            html.Label('Количество порций', htmlFor='recipe-portions'),
+            dcc.Input(
                         placeholder='4',
                         min='1',
                         max='20',
@@ -68,64 +43,38 @@ app.layout = html.Div(className='flex', children=[
                         value='',
                         id='recipe-portions'
                     ),
-                    html.Label('Время приготовления (в минутах)',
-                               htmlFor='recipe-time'),
-                    dcc.Input(
+            html.Label('Время приготовления (в минутах)',
+                       htmlFor='recipe-time'),
+            dcc.Input(
                         placeholder='30',
                         type='number',
                         value='',
                         id='recipe-time'
                     ),
-                    html.Label('Ингредиенты',
-                               htmlFor='recipe-ingredients'),
-                    dcc.Textarea(
+            html.Label('Ингредиенты',
+                       htmlFor='recipe-ingredients'),
+            dcc.Textarea(
                         placeholder='Яйца - 4 штуки\nМолоко - 1 литр',
                         value='',
                         id='recipe-ingredients'
                     ),
-                    html.Label('Рецепт',
-                               htmlFor='recipe-steps'),
-                    dcc.Textarea(
+            html.Label('Рецепт',
+                       htmlFor='recipe-steps'),
+            dcc.Textarea(
                         placeholder='1. Разбить яйца\n2. Налить молоко\n3. Жарить',
                         value='',
                         id='recipe-steps'
                     ),
-                    html.Button('Добавить', id='add-button')
-                ])
-            ]),
-            html.Div(id='user-recipes-container', children=[
-
-            ])
+            html.Button('Добавить', id='add-button')
         ])
+    ]),
+    html.Div(id='user-recipes-panel', children=[
+    dcc.Input(id='recipe-search', placeholder='Найти блюдо',
+              type='text', value=''),
+    html.Div(id='user-recipes-container', children=[
+    ])
     ])
 ])
-
-
-@app.callback(Output('found-recipes-container', 'children'), [Input('recipe-search', 'value')])
-def recipes(search):
-    if len(search) == 0:
-        return html.Div(id='recipes', children=[recipe(val) for val in showcase_recipes])
-    else:
-        return []
-
-
-def recipe(val):
-    time = ''
-    if 'time' in val:
-        time = val['time']
-
-    return html.Div(className='recipe', children=[
-        html.Img(src=val['image']),
-        html.Div(className='recipe-text', children=[
-            html.H5(children=val['title']),
-            html.P(className='recipe-meta',
-                   children=val['portions'] + '  ' + time),
-            html.Details([
-                html.Summary(val['ingredientsAmount']),
-                html.P('Content2')
-            ])
-        ])
-    ])
 
 
 @app.callback(Output('user-recipe-placeholder', 'children'), [Input('recipe-title', 'value'),
@@ -167,6 +116,7 @@ def getTime(val, default):
     else:
         return str(val) + ' минут'
 
+
 def getPortions(val, default):
     if val is None or val == '':
         return default
@@ -179,11 +129,30 @@ def getPortions(val, default):
         else:
             return str(val) + ' порций'
 
-@app.callback(Output('user-recipes-container', 'children'), [Input('recipe-title', 'value')])
+
+@app.callback(Output('user-recipes-container', 'children'), [Input('recipe-search', 'value')])
 def userRecipes(title):
-    for x in recipesCol.find():
-        print(x)
     return html.Div(id='user-recipes', children=[recipe(val) for val in recipesCol.find()])
+
+
+def recipe(val):
+    time = ''
+    if 'time' in val:
+        time = val['time']
+
+    return html.Div(className='recipe', children=[
+        html.Img(src=val['image']),
+        html.Div(className='recipe-text', children=[
+            html.H5(children=val['title']),
+            html.P(className='recipe-meta',
+                   children=val['portions'] + '  ' + time),
+            html.Details([
+                html.Summary(val['ingredientsAmount']),
+                html.P('Content2')
+            ])
+        ])
+    ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
